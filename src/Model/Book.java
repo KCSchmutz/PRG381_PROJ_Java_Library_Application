59b2a263fde0ad8b3702eed17f460126e8ad0a93
Sarelda Nel <141139618+simonscrambled@users.noreply.github.com>
2024-08-24 09:56:43 +0200

    Cleaner code and action methods to be edited

--- src/Model/Book.java
+++ src/Model/Book.java
@@ -1,21 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Model;
 
-/**
- *
- * @author wasch
- */
 public class Book {
     private String authorName;
     private String bookTitle;
     
     public Book(String authorName,String bookTitle){
         this.authorName = authorName;
-        this.bookTitle = bookTitle;
-        
+        this.bookTitle = bookTitle;   
     }
     
     public String getAuthor(){
@@ -25,12 +16,7 @@ public class Book {
     public String getTitle(){
         return bookTitle;
     }
-    
-    public Book(String author, String book){
-        this.authorName = author;
-        this.bookTitle = book;
-    }
-    
+        
     @Override
     public String toString(){
         return "Title: "+bookTitle+", Author: "+authorName;
